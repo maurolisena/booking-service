82673f04dafa8a9bc74faf9f13fda6c693e2176c
Mauro Lisena <maurolisena@gmail.com>
2025-04-11 03:40:24 -0300

    Adding exceptions to handler

--- src/main/java/com/mlisena/booking/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/mlisena/booking/exception/handler/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.mlisena.booking.exception.handler;
 
 import com.mlisena.booking.exception.common.ErrorResponse;
 import com.mlisena.booking.exception.common.NotFoundException;
-import org.apache.commons.logging.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,10 +14,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleProductNotFound(NotFoundException ex, WebRequest request) {
-        Log logger = org.apache.commons.logging.LogFactory.getLog(GlobalExceptionHandler.class);
-        logger.error("Global Exception Handler -> Product not found: {}", ex);
         String path = ((ServletWebRequest) request).getRequest().getRequestURI();
         ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.NOT_FOUND, path);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, path);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.INTERNAL_SERVER_ERROR, path);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
